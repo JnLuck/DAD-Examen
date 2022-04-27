af517c264f51c7e9ba89583afc20276cabc7c8c5
jnluck <jhoselyn.quispe@upeu.edu.pe>
2022-04-27 16:39:00 -0500

    examen 1

---
+++ src/upeu/edu/pe/question1/Almacen.java
@@ -0,0 +1,17 @@
+package upeu.edu.pe.question1;
+
+public class Almacen {
+	
+		private int numProductos;
+	    public Almacen(int nProductos){
+	            this.numProductos=nProductos;
+	    }
+	    public boolean cogerProducto(){
+	            if (this.numProductos>0){
+	                    this.numProductos--;
+	                    return true;
+	            }
+	            return false;
+	    }
+
+}
---
+++ src/upeu/edu/pe/question3/Utilidades.java
@@ -0,0 +1,16 @@
+package upeu.edu.pe.question3;
+
+public class Utilidades {
+
+	public static int numAzar(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void esperarTiempoAzar(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
